19a2f36922c548e9b4a6a986deaa46edc50aa200
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-04-20 18:06:01 +0530

    create navigation layout. Create Required Fragments.

---
+++ app/src/main/java/com/rohitjakhar/greenlightplanettask/AreaFragment.kt
@@ -0,0 +1,18 @@
+package com.rohitjakhar.greenlightplanettask
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AreaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_area, container, false)
+    }
+}
---
+++ app/src/main/java/com/rohitjakhar/greenlightplanettask/ZonePerformanceFragment.kt
@@ -0,0 +1,19 @@
+package com.rohitjakhar.greenlightplanettask
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ZonePerformanceFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_zone_performance, container, false)
+    }
+}
