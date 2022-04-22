a329dbe19df676776fd91e16d7754fa0d0a0d409
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-04-22 11:52:24 +0530

    Complete Search View Flow.

--- app/src/main/java/com/rohitjakhar/greenlightplanettask/ui/fragment/CitizensFragment.kt
+++ app/src/main/java/com/rohitjakhar/greenlightplanettask/ui/fragment/CitizensFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.rohitjakhar.greenlightplanettask.R
 import com.rohitjakhar.greenlightplanettask.databinding.FragmentCitizensBinding
+import com.rohitjakhar.greenlightplanettask.domain.model.GenericModel
 import com.rohitjakhar.greenlightplanettask.ui.adapter.GenericAdapter
 import com.rohitjakhar.greenlightplanettask.ui.viewmodel.CitizenViewModel
 import com.rohitjakhar.greenlightplanettask.utils.Resource
@@ -23,10 +27,12 @@ class CitizensFragment : Fragment() {
     private var _binding: FragmentCitizensBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModels<CitizenViewModel>()
+    private val navArgs: CitizensFragmentArgs by navArgs()
     private val mAdapter by lazy {
         GenericAdapter {
         }
     }
+    private val citizenList = mutableListOf<GenericModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,8 +46,13 @@ class CitizensFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
         initRecyclerView()
         collectData()
+    }
+
+    private fun initView() = binding.apply {
+        tvTitle.text = resources.getString(R.string.performance, navArgs.name)
     }
 
     override fun onResume() {
@@ -53,6 +64,21 @@ class CitizensFragment : Fragment() {
         includeList.tvHeader.setOnClickListener {
             mAdapter.submitList(mAdapter.currentList.reversed())
         }
+        searchName.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let { text ->
+                    mAdapter.submitList(citizenList.filter { it.name.contains(text, true) })
+                }
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let { text ->
+                    mAdapter.submitList(citizenList.filter { it.name.contains(text, true) })
+                } ?: mAdapter.submitList(citizenList)
+                return true
+            }
+        })
     }
 
     private fun collectData() {
@@ -65,7 +91,8 @@ class CitizensFragment : Fragment() {
                         is Resource.Loading -> {
                         }
                         is Resource.Success -> {
-                            mAdapter.submitList(it.data!!.sortedBy { it.name })
+                            citizenList.addAll(it.data!!.sortedBy { it.name })
+                            mAdapter.submitList(citizenList)
                         }
                     }
                 }
